a634840508e5ffb16ccda3e2cc10705f32c01b5e
Lesly Villalobos <lvtdesarrollo@gmail.com>
2020-09-10 21:55:53 -0500

    update

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 require('dotenv/config');
+var urlorigin = '';
+if(window.location.host=='localhost:8081'){
+  urlorigin="http://localhost:3000/api/";
+}else{
+  urlorigin="http://157.245.233.151:3000/api/";
+}
 export default axios.create({
-  baseURL: process.env.URL_API,
+  baseURL: urlorigin,
   headers: {
     "Content-type": "application/json"
   }
